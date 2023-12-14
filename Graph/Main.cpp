e920a7c7e96523737b7d95a16d10f5aac0e81909
to024376 <talha.ozer@ozu.edu.tr>
2023-12-14 16:19:40 +0300

    Somethings wrong I Can feel it

--- Graph/Main.cpp
+++ Graph/Main.cpp
@@ -1,5 +1,20 @@
 //
 // Created by to024376 on 12/14/2023.
 //
+#include <iostream>
+#include <fstream>
+#include <string>
 
-int main(){ return 0;}
+int main(){
+    std::ifstream dict ("shopping_list.txt");
+    std::string zort;
+
+    if (dict.is_open()){
+        while (dict){
+            std::getline(dict,zort);
+        }
+    }
+
+
+
+    return 0;}
